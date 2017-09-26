c1938c85fe7ca14f46e6c4ca92929bba8843b086
Kiran <u1143683@utah.edu>
2017-09-25 23:19:26 -0600

    "In Process"

--- Solution/viterbi.py
+++ Solution/viterbi.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3.6
 import sys
+import numpy as np
+import pandas as pd
+from math import log
 
 prob_file = sys.argv[1]
-sent_file = sys.argv[2]
+words_file = sys.argv[2]
 
-pos_tags =  ["noun", "verb", "inf", "prep"]
+pos_tags = ["noun", "verb", "inf", "prep", "phi"]
 
 prob_matrix = {}
 
@@ -17,7 +20,69 @@ with open(prob_file) as f:
         prob_matrix[(arr[0], arr[1])] = float(arr[2])
 
 for x in prob_matrix:
-    if x[0] in pos_tags and x[1]  in pos_tags:
+    if x[0] in pos_tags and x[1] in pos_tags:
         transition_matrix[x] = prob_matrix[x]
     else:
         emission_matrix[x] = prob_matrix[x]
+
+words = "bears fish".split(' ')
+
+
+def viterbi(words, pos_tags):
+    word_dict = {}
+
+    c = 0
+    for w in words:
+        word_dict[c] = w
+        w = c
+        c += 1
+    words = list(word_dict.keys())
+    print(words)
+
+    col_length = len(words)
+    row_length = len(pos_tags)
+    score = pd.DataFrame(np.zeros(col_length*row_length).reshape(row_length, col_length), index = pos_tags, columns = words)
+    backpointers = score.copy()
+
+    # Initialization
+    w = words[0]
+    for t in pos_tags:
+        pw = emission_matrix.get((word_dict[w], t))
+        if pw is None:
+            pw = 0.0001
+
+        pt = transition_matrix.get((t, "phi"))
+        if pt is None:
+            pt = 0.0001
+
+        score.at[t, w] = log(pw, 2) + log(pt, 2)
+        backpointers.at[t, w] = 0
+
+    # Iteration
+    for i, w in enumerate(words):
+        if i != 0:
+            w_prev = words[i-1]
+            for j, t in enumerate(pos_tags):
+                # score_list = score.ix[:,w_prev].tolist()
+
+                # prev_t = t
+                # if j!=0:
+                #     prev_t = pos_tags[j-1]
+
+                # tag_list = [transition_matrix[x] for x in transition_matrix.keys() if x[1] == prev_t]
+
+                # max_sum = [(x,y) for x in score_list for y in tag_list]
+                # max_sum = [x[0] + log(x[1],2) for x in max_sum]
+                # max_sum = max(max_sum)
+
+                pw = emission_matrix.get((word_dict[w], t))
+                if pw is None:
+                    pw = 0.0001
+
+                # score.at[t, w] = log(pw, 2) + max_sum
+
+    # print(backpointers)
+    print(score)
+
+
+viterbi(words, pos_tags)
